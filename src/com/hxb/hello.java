37b4ea00e5faccbc03695faf9639a43a98c94a37
hxb227797 <2277978888@qq.com>
2020-10-21 22:57:54 +0800

    Default Changelist

---
+++ src/com/hxb/hello.java
@@ -0,0 +1,12 @@
+package com.hxb;
+
+public class hello {
+    public static void main(String[] args) {
+         int i = 1;
+         int j = 5;
+         if (i>=j){
+             System.out.println(3);
+         }
+        System.out.println("1");
+    }
+}
